0ee19a2556b8d8028b95f84b4dc4e37623603f63
alonghuayu <alonghuayu@163.com>
2024-11-24 22:32:47 +0800

    守卫

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,25 +1,39 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { CatsController } from './cats/cats.controller';
-import { logger } from './common/middleware/log.middleware';
-import { HttpExceptionFilter } from './filter/http-exception.filter';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CatsModule } from "./cats/cats.module";
+import { LoggerMiddleware } from "./common/middleware/logger.middleware";
+import { CatsController } from "./cats/cats.controller";
+import { logger } from "./common/middleware/log.middleware";
+import { HttpExceptionFilter } from "./filter/http-exception.filter";
+import { RolesGuard } from "./guard/roles.guard";
+import { APP_GUARD, APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: 'APP_FILTER',
-    useClass: HttpExceptionFilter
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware, logger)
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
-      // .forRoutes(CatsController);
+      .forRoutes({ path: "*", method: RequestMethod.ALL });
+    // .forRoutes(CatsController);
   }
 }
